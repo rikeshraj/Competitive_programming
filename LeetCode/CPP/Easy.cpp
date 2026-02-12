fb17979d126e6e9a7ff31a8f9d04055b51708e8f
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-02-13 00:07:11 +0530

    Update Easy.cpp

--- LeetCode/CPP/Easy.cpp
+++ LeetCode/CPP/Easy.cpp
@@ -1,4 +1,4 @@
-9. PalindromeNumber
+//9. PalindromeNumber
 class Solution {
 public:
     bool isPalindrome(int x) {
@@ -11,3 +11,47 @@ public:
         return false;
     }
 };
+
+
+//110. Balanced Binary Tree
+class Solution {
+private: 
+    int checkHeight(TreeNode* node) {
+        if(!node) {
+            return 0;
+        } 
+        int leftHeight = checkHeight(node->left); 
+        if(leftHeight == -1) return -1; 
+        int rightHeight = checkHeight(node->right); 
+        if(rightHeight == -1) return -1; 
+        if(abs(leftHeight-rightHeight)>1) return -1; 
+        return std::max(leftHeight, rightHeight)+1;
+    }
+public: 
+    bool isBalanced(TreeNode* root) {
+        return checkHeight(root) != -1;
+    }
+};
+
+
+//3010. Divide an Array Into Subarrays With Minimum Cost I 
+lass Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        int n = nums.size();
+        int first = nums[0];
+        int min1 = INT_MAX, min2 = INT_MAX;
+        for(int i=1; i<n; ++i) {
+            if(nums[i]<min1) {
+                min2 = min1;
+                min1 = nums[i];
+            }
+            else if(nums[i]<min2) {
+                min2 = nums[i];
+            }
+        }
+        return first+min1+min2;
+    }
+};
+
+

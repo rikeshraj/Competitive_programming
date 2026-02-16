26fffb2428f10bd8b864e668186d3dfda15226f7
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-02-16 20:27:47 +0530

    Update Easy.java

--- LeetCode/Java/Easy.java
+++ LeetCode/Java/Easy.java
@@ -1,3 +1,19 @@
+// 1. Two Sum 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        java.util.HashMap<Integer, Integer> map = new java.util.HashMap<>();
+        for(int i=0; i<nums.length; i++) {
+            int com = target-nums[i];
+            if(map.containsKey(com)) { 
+                return new int[] {map.get(com), i};
+            }
+            map.put(nums[i], i);
+        }
+            return new int[] {};
+    }
+}
+
+
 //190. Reverse Bits
 class Solution {
     public int reverseBits(int n) {

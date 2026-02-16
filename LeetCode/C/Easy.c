440d3830b7cae9d03f1074064fc64baf14b22e07
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-02-16 20:17:50 +0530

    Update Easy.c

--- LeetCode/C/Easy.c
+++ LeetCode/C/Easy.c
@@ -1,3 +1,20 @@
+// 1. Two Sum
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int* res = (int*)malloc(2*sizeof(int));
+    *returnSize = 2;
+    for(int i=0; i<numsSize; i++) {
+        for(int j=i+1; j<numsSize; j++) {
+            if(nums[i]+nums[j] == target) {
+                res[0] = i;
+                res[1] = j;
+                return res;
+            }
+        }
+    }
+    return NULL;
+}
+
+
 // 190. Reverse Bits
 int reverseBits(int n) {
     int res = 0;

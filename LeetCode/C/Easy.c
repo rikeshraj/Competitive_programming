b8e7ebf89669624b4114147c1cd478938572fc51
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-02-16 20:02:38 +0530

    Add reverseBits function in Easy.c
    Implement reverseBits function to reverse the bits of an integer.

---
+++ LeetCode/C/Easy.c
@@ -0,0 +1,16 @@
+// 190. Reverse Bits
+int reverseBits(int n) {
+    int res = 0;
+    for(int i=0; i<32; ++i) {
+        res <<= 1;
+        res |= (n&1);
+        n >>= 1;
+    }
+    return res;
+}
+// OR 
+for(int i=0; i<32; ++i) {
+    res = (res<<1) | (n&1);
+    n >>= 1;
+}
+

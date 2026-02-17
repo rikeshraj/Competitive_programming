0513287cfb1764a1e278f84012e855322dd2beed
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-02-17 19:21:15 +0530

    Update Pandas.py

--- LeetCode/Pandas.py
+++ LeetCode/Pandas.py
@@ -14,3 +14,22 @@ def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
 # 2880. Select Data
 def selectData(students: pd.DataFrame) -> pd.DataFrame:
     return students[students['student_id'] == 101][['name', 'age']]
+
+# 2881. Create a New Column
+def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['bonus'] = employees['salary']*2
+    return employees
+ 
+# 2882. Drop Duplicate Rows
+def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
+    return customers.drop_duplicates(subset='email', keep='first')
+ 
+# 2883. Drop Missing Data
+def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
+    return students.dropna(subset='name')
+ 
+# 2884. Modify Columns
+def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['salary'] = employees['salary']*2
+    return employees
+

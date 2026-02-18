3e822bee778e11d0d533d986365903c44139bf29
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-02-18 17:48:56 +0530

    Update Easy.cpp

--- LeetCode/CPP/Easy.cpp
+++ LeetCode/CPP/Easy.cpp
@@ -80,6 +80,32 @@ public:
     }
 };
 
+// 401. Binary Watch 
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> res; 
+        for(int h=0; h<12; h++) {
+            for(int m=0; m<60; m++) {
+                if(__builtin_popcount(h) + __builtin_popcount(m) == turnedOn) {
+                    res.push_back(to_string(h) + (m<10 ? ":0" : ":") + to_string(m));
+                }
+            }
+        }
+        return res;
+    }
+};
+
+
+// 693. Binary Number with Alternating Bits
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        unsigned int res = (unsigned int)n ^ ((unsigned int)n >> 1);
+        return (res & (res+1)) == 0;
+    }
+};
+
 
 // 3010. Divide an Array Into Subarrays With Minimum Cost I 
 class Solution {

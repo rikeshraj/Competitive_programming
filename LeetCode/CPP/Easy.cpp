b729aa865a175f37285f1728767ec7ec06209e9d
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-02-20 21:48:14 +0530

    Update Easy.cpp

--- LeetCode/CPP/Easy.cpp
+++ LeetCode/CPP/Easy.cpp
@@ -127,6 +127,34 @@ int countBinarySubstrings(char* s) {
 }
 
 
+// 761. Special Binary String
+class Solution {
+public:
+    string makeLargestSpecial(string s) {
+        int count = 0; 
+        int i = 0; 
+        vector<string> res; 
+        for(int j=0; j<s.length(); j++) {
+            if(s[j] == '1') {
+                count ++; 
+            } else {
+                count--;
+            }
+            if(count == 0) {
+                res.push_back("1" + makeLargestSpecial(s.substr(i+1, j-i-1)) + "0");
+                i = j+1;
+            }
+        }
+        sort(res.begin(), res.end(), greater<string>());
+        string finalResult = "";
+        for(const string str : res) {
+            finalResult += str;
+        }
+        return finalResult; 
+    }
+};
+
+
 // 3010. Divide an Array Into Subarrays With Minimum Cost I 
 class Solution {
 public:

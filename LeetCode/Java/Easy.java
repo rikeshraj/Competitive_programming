cc5c082cdd45eb8f58146a072cbd4a27e778b350
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-02-20 21:48:56 +0530

    Update Easy.java

--- LeetCode/Java/Easy.java
+++ LeetCode/Java/Easy.java
@@ -114,6 +114,33 @@ class Solution {
 }
 
 
+// 761. Special Binary String
+class Solution {
+    public String makeLargestSpecial(String s) {
+        int count = 0; 
+        int i = 0; 
+        List<String> res = new ArrayList<>();
+        for(int j=0; j<s.length(); j++) {
+            if(s.charAt(j) == '1') {
+                count++;
+            } else {
+                count--;
+            }
+            if(count == 0) {
+                res.add('1' + makeLargestSpecial(s.substring(i+1, j)) + "0");
+                i = j+1;
+            }
+        }
+        Collections.sort(res, Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+        for(String str : res) {
+            sb.append(str);
+        }
+        return sb.toString();
+    }
+}
+
+
 // 3010. Divide an Array Into Subarrays With Minimum Cost I 
 class Solution {
     public int minimumCost(int[] nums) {

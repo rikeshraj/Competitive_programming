538302e3e222b4151d07a4da0bb3ce3956545925
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-02-22 00:31:15 +0530

    Update Easy.py

--- LeetCode/Python/Easy.py
+++ LeetCode/Python/Easy.py
@@ -97,6 +97,29 @@ class Solution:
         return count
 
 
+# 762. Prime Number of Set Bits in Binary Representation
+class Solution(object):
+    def countPrimeSetBits(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        def is_prime(n):
+            if n<= 1:
+                return False
+            for i in range(2, int(n**0.5) + 1):
+                if n%i == 0:
+                    return False
+            return True
+        count = 0
+        for i in range(left, right+1):
+            set_bits = bin(i).count('1')
+            if is_prime(set_bits):
+                count += 1
+        return count
+
+
 # 3010. Divide an Array Into Subarrays With Minimum Cost I 
 class Solution:
     def minimumCost(self, nums: List[int]) -> int:

1142c595bfad93130b60e4172b3476a49e9c4a7b
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-03-12 22:40:22 +0530

    Fix carry calculation in linked list addition

--- LeetCode/C/Medium.c
+++ LeetCode/C/Medium.c
@@ -9,7 +9,7 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
         int val1 = l1 != NULL ? l1->val : 0;
         int val2 = l2 != NULL ? l2->val : 0;
         int total = val1 + val2 + carry; 
-        carry = total/10; 
+        carry = total/10;
         current->next = (struct ListNode*)malloc(sizeof(struct ListNode));
         current->next->val = total%10; 
         current->next->next = NULL; 

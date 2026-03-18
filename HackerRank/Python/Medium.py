5cfc8db7b0561e5c2486688f75c4cdd002791b19
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-03-18 21:47:03 +0530

    Update Medium.py

--- HackerRank/Python/Medium.py
+++ HackerRank/Python/Medium.py
@@ -1,13 +1,13 @@
-#Write Function
+# Write Function
 def is_leap(year):
   leap = False
   # Write your logic here
   if year%4==0 and year%100 != 0 or year%400 == 0:
-    leap = True
+    leap = True 
   return leap
 
 
-#Minion Game
+# Minion Game
 
 
 

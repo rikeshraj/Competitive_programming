fb74c75b60a411f6a3f88d8b996c8e595cab0b50
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-03-23 19:47:52 +0530

    Update Easy.py

--- LeetCode/Python/Easy.py
+++ LeetCode/Python/Easy.py
@@ -19,6 +19,25 @@ def isPalindrome(self, x: int) -> bool:
     return copy == rev
 
 
+# 13. Roman to Integer
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        total = 0
+        prev = 0
+        for ch in reversed(s):
+            val = roman[ch]
+            if val < prev:
+                total -= val
+            else:
+                total += val
+                prev = val
+        return total
+
+
 # 110. Balanced Binary Tree
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:

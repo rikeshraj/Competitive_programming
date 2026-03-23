5e6427d969304294328f7ca2e6ba2b80314a2d61
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-03-23 19:45:56 +0530

    Update Easy.c

--- LeetCode/C/Easy.c
+++ LeetCode/C/Easy.c
@@ -27,6 +27,35 @@ bool isPalindrome(int x) {
 }
 
 
+// 13. Roman to Integer
+int value(char c) {
+    switch (c) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default:  return 0;  // invalid character
+    }
+}
+int romanToInt(char* s) {
+    int total = 0;
+    int prev = 0;
+    for (int i = 0; s[i] != '\0'; i++) { } 
+    int n = 0;
+    while (s[n] != '\0') n++;
+    for (int i = n - 1; i >= 0; i--) {
+        int curr = value(s[i]);
+        if (curr < prev) total -= curr;
+        else total += curr;
+        prev = curr;
+    }
+    return total;
+}
+
+
 // 110. Balanced Binary Tree
 struct TreeNode {
     int val;

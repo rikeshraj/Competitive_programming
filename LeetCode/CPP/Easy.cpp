59fb9468b1e1b553b1fd2fa2e6be571fde7eef69
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-03-25 19:49:53 +0530

    Update Easy.cpp

--- LeetCode/CPP/Easy.cpp
+++ LeetCode/CPP/Easy.cpp
@@ -71,6 +71,29 @@ public:
 };
 
 
+// 20. Valid Parentheses
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (c=='(' || c=='{' || c=='[') {
+                st.push(c);
+            } else {
+                if (st.empty()) return false;
+                char top = st.top();
+                if ((c==')') && top=='(' || (c=='}') && top=='{' || (c==']') && top=='[') {
+                    st.pop();
+                }else {
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
+
+
 //110. Balanced Binary Tree
 class Solution {
 private: 

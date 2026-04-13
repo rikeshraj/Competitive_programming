d8e2e6f94d2d08755affa18a1bc866215013b1e9
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-04-13 21:19:21 +0530

    Update .py

--- LeetCode/Quest/Database/Basic/.py
+++ LeetCode/Quest/Database/Basic/.py
@@ -7,7 +7,7 @@ def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFr
 def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
     manager_salary = employee.set_index("id")["salary"]
     employee["mgr_salary"] = employee["managerId"].map(manager_salary)
-    result = employee.loc[employee["salary"] > employee["mgr_salary"], ["name"]]
+    result = employee.loc[employee["salary"] > employee["mgr_salary"], ["name"]] 
     return result.rename(columns={"name": "Employee"})
  
 # Q3. Not Boring Movies

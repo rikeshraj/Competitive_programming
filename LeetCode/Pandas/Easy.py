9d86017fa3cd9f6057fba3a96d68cb3c72c14940
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-04-19 20:46:45 +0530

    Update Easy.py

--- LeetCode/Pandas/Easy.py
+++ LeetCode/Pandas/Easy.py
@@ -1,7 +1,7 @@
 # 183. Customers Who Never Order
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
     df = customers[~customers['id'].isin(orders['customerId'])]
-    df = df[['name']].rename(columns={'name' : 'Customers'})
+    df = df[['name']].rename(columns={'name' : 'Customers'}) 
     return df
 #OR  
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:

8f3dbfef1e7b412c6a50253e95d407b50d70873d
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-04-20 10:48:54 +0530

    Implement maxDistance method for house colors
    Add solution for 'Two Furthest Houses With Different Colors' problem.

--- LeetCode/Python/Easy.py
+++ LeetCode/Python/Easy.py
@@ -182,6 +182,23 @@ class Solution:
         return max_dis
 
 
+# 2078. Two Furthest Houses With Different Colors
+class Solution:
+    def maxDistance(self, colors: List[int]) -> int:
+        n = len(colors)
+        dist1 = 0
+        for j in range(n-1, -1, -1):
+            if colors[j] != colors[0]:
+                dist1 = j
+                break
+        dist2 = 0
+        for i in range(n):
+            if colors[i] != colors[n-1]:
+                dist2 = (n-1) - i
+                break    
+        return max(dist1, dist2)
+
+
 # 3010. Divide an Array Into Subarrays With Minimum Cost I 
 class Solution:
     def minimumCost(self, nums: List[int]) -> int:

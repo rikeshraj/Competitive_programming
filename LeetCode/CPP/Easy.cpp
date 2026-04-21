96b59c02db120fd2f450bace425de4255c5baff4
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-04-21 23:15:39 +0530

    Add removeDuplicates method to Solution class
    Implement removeDuplicates function to remove duplicates from a sorted array.

--- LeetCode/CPP/Easy.cpp
+++ LeetCode/CPP/Easy.cpp
@@ -116,6 +116,23 @@ public:
 };
 
 
+// 26. Remove Duplicates from Sorted Array
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int k = 1; // index for next unique element
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[k - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
+
+
 //110. Balanced Binary Tree
 class Solution {
 private: 

ce557bc3da3dbe26671325c19f11a1116a6a70a3
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-04-22 12:59:07 +0530

    Update Easy.cpp

--- LeetCode/CPP/Easy.cpp
+++ LeetCode/CPP/Easy.cpp
@@ -133,6 +133,22 @@ public:
 };
 
 
+// 27. Remove Element
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
+
+
 //110. Balanced Binary Tree
 class Solution {
 private: 

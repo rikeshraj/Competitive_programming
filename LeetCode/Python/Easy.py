84f3cb36c2806168515e371053b9c804dc85fa2d
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-04-25 23:45:15 +0530

    Update Easy.py

--- LeetCode/Python/Easy.py
+++ LeetCode/Python/Easy.py
@@ -108,6 +108,18 @@ class Solution:
         return k
 
 
+# 28. Find the Index of the First Occurrence in a String
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        n, m = len(haystack), len(needle)
+        if m == 0:
+            return 0
+        for i in range(n - m + 1):
+            if haystack[i:i+m] == needle:
+                return i
+        return -1
+
+
 # 110. Balanced Binary Tree
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:

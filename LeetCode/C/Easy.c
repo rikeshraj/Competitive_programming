9016d72093eaefb198acbe1bb436fd4a8c5b0667
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-04-26 13:51:40 +0530

    Update Easy.c

--- LeetCode/C/Easy.c
+++ LeetCode/C/Easy.c
@@ -154,6 +154,22 @@ int strStr(char* haystack, char* needle) {
 }
 
 
+// 35. Search Insert Position
+int searchInsert(int* nums, int numsSize, int target) {
+    int left = 0, right = numsSize - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target)
+            return mid;
+        else if (nums[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return left;
+}
+
+
 // 110. Balanced Binary Tree
 struct TreeNode {
     int val;

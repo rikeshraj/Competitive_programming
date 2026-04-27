ca9918e4eb1c3c194fe296d97163827506e4e1e1
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-04-27 21:43:53 +0530

    Update Easy.cpp

--- LeetCode/CPP/Easy.cpp
+++ LeetCode/CPP/Easy.cpp
@@ -187,6 +187,23 @@ public:
 };
 
 
+// 58. Length of Last Word
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.size() - 1;
+        while (i >= 0 && s[i] == ' ')
+            i--;
+        int len = 0;
+        while (i >= 0 && s[i] != ' ') {
+            len++;
+            i--;
+        }
+        return len;
+    }
+};
+
+
 //110. Balanced Binary Tree
 class Solution {
 private: 

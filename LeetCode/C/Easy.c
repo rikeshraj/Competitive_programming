e19d2638496bd4b81abc767c1ab98d028daedd70
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-04-27 21:42:37 +0530

    Add function to calculate length of last word

--- LeetCode/C/Easy.c
+++ LeetCode/C/Easy.c
@@ -170,6 +170,22 @@ int searchInsert(int* nums, int numsSize, int target) {
 }
 
 
+// 58. Length of Last Word
+int lengthOfLastWord(char* s) {
+    int i = 0;
+    while (s[i] != '\0') i++;
+    i--;
+    while (i >= 0 && s[i] == ' ')
+        i--;
+    int len = 0;
+    while (i >= 0 && s[i] != ' ') {
+        len++;
+        i--;
+    }
+    return len;
+}
+
+
 // 110. Balanced Binary Tree
 struct TreeNode {
     int val;

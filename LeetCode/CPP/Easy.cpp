3e4aaebf9717b9437b8b3a9f354f088bb8314544
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-04-30 20:40:02 +0530

    Update Easy.cpp

--- LeetCode/CPP/Easy.cpp
+++ LeetCode/CPP/Easy.cpp
@@ -204,6 +204,23 @@ public:
 };
 
 
+// 66. Plus One
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
+
+
 //110. Balanced Binary Tree
 class Solution {
 private: 

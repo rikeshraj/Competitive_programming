4e78283e69029073fdbd7424536d3dd38efd3a10
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-05-10 22:40:31 +0530

    Refactor makeLargestSpecial method in Hard.java

--- LeetCode/Java/Hard.java
+++ LeetCode/Java/Hard.java
@@ -3,14 +3,14 @@ class Solution {
     public String makeLargestSpecial(String s) {
         int count = 0; 
         int i = 0; 
-        List<String> res = new ArrayList<>();
+        List<String> res = new ArrayList<>(); 
         for(int j=0; j<s.length(); j++) {
             if(s.charAt(j) == '1') {
-                count++;
+                count++; 
             } else {
                 count--;
-            }
-            if(count == 0) {
+            } 
+            if(count == 0) { 
                 res.add('1' + makeLargestSpecial(s.substring(i+1, j)) + "0");
                 i = j+1;
             }

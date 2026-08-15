625a07dbd5a8f90b516ada8ee93806353301b75d
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-08-15 10:20:32 +0530

    Add LRUCache class implementation
    Implemented LRU Cache class with get and put methods.

--- LeetCode/Pandas/Medium.py
+++ LeetCode/Pandas/Medium.py
@@ -1,3 +1,20 @@
+# 146. LRU Cache
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.pop(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
+
 # 176. Second Highest Salary
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
     distinct_sal = employee['salary'].drop_duplicates().sort_values(ascending=False)

4bcf0ad34d66e7cd67c6c15b8d0119b4c4aeaa95
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-08-15 10:19:30 +0530

    Implement LRU Cache class in Medium.cpp

--- LeetCode/CPP/Medium.cpp
+++ LeetCode/CPP/Medium.cpp
@@ -23,6 +23,41 @@ public:
 }; 
 
 
+// 146. LRU Cache
+class LRUCache {
+private:
+    int capacity;
+    list<pair<int, int>> cache;
+    unordered_map<int, list<pair<int, int>>::iterator> mp;
+public:
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+    }
+    int get(int key) {
+        if (mp.find(key) == mp.end()) return -1;
+        auto it = mp[key];
+        int value = it->second;
+        cache.splice(cache.begin(), cache, it);
+        return value;        
+    }
+    void put(int key, int value) {
+        if (mp.find(key) != mp.end()) {
+            auto it = mp[key];
+            it->second = value;
+            cache.splice(cache.begin(), cache, it);
+            return;
+        }
+        cache.push_front({key, value});
+        mp[key] = cache.begin();
+        if (cache.size() > capacity) {
+            auto last = cache.back();
+            mp.erase(last.first);
+            cache.pop_back();
+        }
+    }
+};
+
+
 // 1461. Check If a String Contains All Binary Codes of Size K
 class Solution {
 public:

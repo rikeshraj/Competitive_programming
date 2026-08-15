bc1e1c799de2b0568180c6b432f14180b5d3bdd8
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-08-15 10:11:59 +0530

    Add LRUCache implementation in .cpp file
    Implement LRUCache class with get and put methods.

---
+++ LeetCode/Quest/2026SpringSprint/Practice1/.cpp
@@ -0,0 +1,52 @@
+/*
+Q1. LRU Cache
+Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+Implement the LRUCache class:
+LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+int get(int key) Return the value of the key if the key exists, otherwise return -1.
+void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+The functions get and put must each run in O(1) average time complexity.
+*/
+class LRUCache {
+private:
+    int capacity;
+    list<pair<int, int>> cache;
+    unordered_map<int, list<pair<int, int>>::iterator> mp;
+public:
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+    }
+    int get(int key) {
+        if (mp.find(key) == mp.end()) return -1;
+        auto it = mp[key];
+        int value = it->second;
+        cache.splice(cache.begin(), cache, it);
+        return value;        
+    }
+    void put(int key, int value) {
+        if (mp.find(key) != mp.end()) {
+            auto it = mp[key];
+            it->second = value;
+            cache.splice(cache.begin(), cache, it);
+            return;
+        }
+        cache.push_front({key, value});
+        mp[key] = cache.begin();
+        if (cache.size() > capacity) {
+            auto last = cache.back();
+            mp.erase(last.first);
+            cache.pop_back();
+        }
+    }
+};
+
+
+/*
+
+*/
+
+
+
+/*
+
+*/

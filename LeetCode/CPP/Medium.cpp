aa02a828d4aec3919eb57244eaf59d6c56b30309
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-08-18 17:07:45 +0530

    Update Medium.cpp

--- LeetCode/CPP/Medium.cpp
+++ LeetCode/CPP/Medium.cpp
@@ -22,6 +22,33 @@ public:
     }
 }; 
 
+// 22. Generate Parentheses
+class Solution {
+public:
+    vector<string> result;
+    void backtrack(string& s, int open, int close, int n) {
+        if (s.size() == 2 * n) {
+            result.push_back(s);
+            return;
+        }
+        if (open < n) {
+            s.push_back('(');
+            backtrack(s, open + 1, close, n);
+            s.pop_back();
+        }
+        if (close < open) {
+            s.push_back(')');
+            backtrack(s, open, close + 1, n);
+            s.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        string s;
+        backtrack(s, 0, 0, n);
+        return result;
+    }
+};
+
 // 56. Merge Intervals
 class Solution {
 public:
@@ -91,6 +118,86 @@ public:
         return prev1;
     }
 };
+
+// 200. Number of Islands
+class Solution {
+public:
+    void dfs(vector<vector<char>>& grid, int r, int c) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        if (r < 0 || r >= rows ||
+            c < 0 || c >= cols ||
+            grid[r][c] != '1') {
+            return;
+        }
+        grid[r][c] = '0';
+        dfs(grid, r + 1, c);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r, c - 1);
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        if (grid.empty())
+            return 0;
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int islands = 0;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == '1') {
+                    islands++;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+        return islands;
+    }
+};
+
+// 994. Rotting Oranges
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        queue<pair<int, int>> q;
+        int fresh = 0;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == 2) {
+                    q.push({r, c});
+                }
+                else if (grid[r][c] == 1) {
+                    fresh++;
+                }
+            }
+        }
+        int minutes = 0;
+        int dr[] = {1, -1, 0, 0};
+        int dc[] = {0, 0, 1, -1};
+        while (!q.empty() && fresh > 0) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                auto [r, c] = q.front();
+                q.pop();
+                for (int d = 0; d < 4; d++) {
+                    int nr = r + dr[d];
+                    int nc = c + dc[d];
+                    if (nr >= 0 && nr < rows &&
+                        nc >= 0 && nc < cols &&
+                        grid[nr][nc] == 1) {
+                        grid[nr][nc] = 2;
+                        fresh--;
+                        q.push({nr, nc});
+                    }
+                }
+            }
+            minutes++;
+        }
+        return fresh == 0 ? minutes : -1;
+    }
+};
+
 
 // 1461. Check If a String Contains All Binary Codes of Size K
 class Solution {

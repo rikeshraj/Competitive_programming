a1446b52a17c54c1cbd69c96978b779aa4ef30de
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-08-19 17:50:23 +0530

    Add maxProfit function for stock price analysis
    Implemented maxProfit function to calculate maximum profit from stock prices.

--- LeetCode/CPP/Easy.cpp
+++ LeetCode/CPP/Easy.cpp
@@ -242,6 +242,29 @@ public:
 };
 
 
+// 121. Best Time to Buy and Sell Stock
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+
+            minPrice = min(minPrice, price);
+
+            maxProfit = max(
+                maxProfit,
+                price - minPrice
+            );
+        }
+
+        return maxProfit;
+    }
+};
+
+
 // 190. Reverse Bits
 class Solution {
 public:

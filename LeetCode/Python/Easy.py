56e975c18217cb582bb6562526cb701881b86378
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-08-19 17:51:06 +0530

    Add maxProfit function for stock trading problem
    Implemented maxProfit function to calculate maximum profit from stock prices.

--- LeetCode/Python/Easy.py
+++ LeetCode/Python/Easy.py
@@ -177,6 +177,20 @@ class Solution:
         return check_height(root) != -1
 
 
+# 121. Best Time to Buy and Sell Stock
+class Solution:
+    def maxProfit(self, prices):
+        min_price = float('inf')
+        max_profit = 0
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(
+                max_profit,
+                price - min_price
+            )
+        return max_profit
+
+
 # 190. Reverse Bits
 class Solution:
     def reverseBits(self, n: int) -> int:

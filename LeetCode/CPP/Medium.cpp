3d869e85595b928edd5209ae1e2da8e3a1770a22
Rikesh Raj <rikesh.raj.247@gmail.com>
2026-08-26 22:24:34 +0530

    Add solution for Top K Frequent Elements problem

--- LeetCode/CPP/Medium.cpp
+++ LeetCode/CPP/Medium.cpp
@@ -245,6 +245,34 @@ public:
     }
 };
 
+// 347. Top K Frequent Elements
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;
+        }
+        priority_queue<
+            pair<int, int>,
+            vector<pair<int, int>>,
+            greater<pair<int, int>>
+        > pq;
+        for (auto& [num, count] : freq) {
+            pq.push({count, num});
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+        vector<int> result;
+        while (!pq.empty()) {
+            result.push_back(pq.top().second);
+            pq.pop();
+        }
+        return result;
+    }
+};
+
 // 560. Subarray Sum Equals K
 class Solution {
 public:
